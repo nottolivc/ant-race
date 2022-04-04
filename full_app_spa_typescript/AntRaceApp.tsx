b48389fd22c316ac027d299e53c24a554c71e722
Andrew S <nottolivc@gmail.com>
2022-04-03 17:22:10 -0700

    include folder with basic tsx version of app and sandbox

--- full_app_spa_typescript/AntRaceApp.tsx
+++ full_app_spa_typescript/AntRaceApp.tsx
@@ -1,46 +1,63 @@
 import React, { useState } from 'react';
-import seedData from '../api/data.json';
+//import seedData from '../src/api/data.json';
 import '../App.css';
 
-const AntRacer = () => {
+const AntRaceApp = () => {
 
   const [ants, setAnts] = useState({});
   // eslint-disable-next-line
   const [updateAnts, setUpdateAnts] = useState({});
-
-  const calculateOdds = (): void => {
-    function updateAnts(chances:any, idx:any):void {
-      const updateAnts = Object.assign({}, {ants});
+  
+  // needs all any type changed if possible to strictest type/interface declaration 
+  // This is a Full Lean MVP version of the Entire App in one TypeScript/TSX file
+  // Initial basic version runs HERE: https://codesandbox.io/s/react-typescript-forked-vguprc?file=/src/App.tsx
+  const calculateOdds = () => {
+    function updateAnts(chances:any, idx:any) {
+      const updateAnts:any = Object.assign({}, {ants});
       updateAnts.ants[idx].likelihoodOfAntWinning = chances;
       updateAnts.ants[idx].calculatingOdds = "Calculating...";
       setUpdateAnts(updateAnts);
     }
 
-    function generateAntWinLikelihoodCalculator():void {
+    function generateAntWinLikelihoodCalculator() {
       var delay = 7000 + Math.random() * 7000;
       var likelihoodOfAntWinning = Math.random();
-      return function(callback:any):void {
+      return function(callback:any) {
         setTimeout(function() {
           callback(likelihoodOfAntWinning);
         }, delay);
       };
     }
-    // eslint-disable-next-line
+    
     Object.keys(ants).map((idx) => {
       updateAnts('', idx);
-      const callback = (likelihoodOfAntWinning) => {
+      const callback = (likelihoodOfAntWinning:any) => {
         updateAnts(likelihoodOfAntWinning, idx);
       };
       generateAntWinLikelihoodCalculator()(callback);
     });
   }
 
+    const loadData = () => {
+    const ants:any = {};
+    const fetchData = async () => {
+      const result = seedData;
+      result.ants.forEach((ant:any, idx:any) => {
+        ants[idx] = ant;
+        ants[idx].likelihoodOfAntWinning = "";
+        ants[idx].calculatingOdds = "";
+      });      
+      setAnts(ants);
+    };
+    fetchData();
+  };
+
   const AntList = () => {
     return antBuilder();
   }
 
   const antBuilder = () => {
-      let ant = Object.keys(ants).sort((a, b) => ants[a].likelihoodOfAntWinning - ants[b].likelihoodOfAntWinning).reverse().map((idx) => {
+      let ant:any = Object.keys(ants).sort((a:any, b:any) => ants[a].likelihoodOfAntWinning - ants[b].likelihoodOfAntWinning).reverse().map((idx) => {
       let racerId = "racer" + idx;
       let odds = Math.round(ants[idx].likelihoodOfAntWinning * 100)+"%";
       return (
@@ -58,22 +75,6 @@ const AntRacer = () => {
     return ant;
   }
 
-  const loadData = () => {
-    
-    const ants = {};
-    const fetchData = async () => {
-      const result = seedData;
-      result.ants.forEach((ant, idx) => {
-        ants[idx] = ant;
-        ants[idx].likelihoodOfAntWinning = "";
-        ants[idx].calculatingOdds = "";
-      });      
-      setAnts(ants);
-    };
-    fetchData();
-  };
-
-
   return ( 
     <div>
       <button type="button" onClick={loadData}>Load Ant Data</button>
@@ -84,4 +85,4 @@ const AntRacer = () => {
 };
 
 
-export default AntRacer;
+export default AntRaceApp;
